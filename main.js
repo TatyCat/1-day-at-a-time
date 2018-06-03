488390d7c430ddd36e388bd94b3797cd9a8c119a
TatyCat <williams.tatyanna@gmail.com>
2018-06-02 23:39:44 -0400

    Weather api for Tampa Added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// DOM Edits
+// DOM Edits / JS
 
 // Greeting by time of day
 let getTimeOfDay = new Date().getHours();
@@ -16,7 +16,7 @@ if ((getTimeOfDay >= 12) && (getTimeOfDay <= 16)) {
 
 if ((getTimeOfDay >= 17) && (getTimeOfDay <= 20)) {
     document.getElementById('greetingtimeOfDay').innerText = "Evening";
-    document.getElementById('welcomeGreeting').style.color = "black";
+    document.getElementById('welcomeGreeting').style.color = "gold";
 }
 if ((getTimeOfDay >= 21) && (getTimeOfDay <= 23)) {
     document.getElementById('greetingtimeOfDay').innerText = "Night";
@@ -26,40 +26,89 @@ if ((getTimeOfDay >= 21) && (getTimeOfDay <= 23)) {
 
 //Display Today's Date
 let getTodaysMonth = new Date().getMonth();
-let getTodaysDay = new Date().getDate();
+let getTodaysDate = new Date().getDate();
+let getTodaysDay = new Date().getDay();
+
 var getTodaysYear = new Date().getFullYear();
-
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-document.getElementById('greetingDate').innerText = months[getTodaysMonth] + " " + getTodaysDay + ", " + getTodaysYear;
+document.getElementById('greetingDate').innerText = weekdays[getTodaysDay] + ", " + months[getTodaysMonth] + " " + getTodaysDate + ", " + getTodaysYear;
 
-
-
-//Get Location & Display city
-
-document.getElementById('cityLocation').innerText = "Tampa, FL";
-document.getElementById('cityLocation').style.color = "gold";
-
-// Weather - TEMP
-document.getElementById("highTemp").innerText = 85;
-document.getElementById("lowTemp").innerText = 75;
 
 //weather Alert Panel *if Statement*
-document.getElementById('weatherAlertPanel').className = "w3-text-white ";
-document.getElementById("weatherAlertPanel").className += " w3-panel w3-red w3-round-large ";
+document.getElementById('weatherAlertPanel').className = "w3-text-white w3-panel w3-red w3-round-large ";
+
 
 
 
 //Top News
 // document.getElementById("topNew").innerHTML = 
 
+// document.getElementById('').style.color = "gold";
+
+//--Yahoo WEATHER API--
+
+var callbackFunction = function (data) {
+    //Yahoo Get Sunrise/Set
+    var astronomy = data.query.results.channel.astronomy;
+    document.getElementById("displaySunRise").innerText = astronomy.sunrise;
+    document.getElementById("displaySunSet").innerText = astronomy.sunset;
+
+    //Yahoo Get Location & Display city
+    var item = data.query.results.channel.item;
+    document.getElementById('cityLocation').innerText = item.title;
+    
+
+    //Yahoo Get Forcast
+
+    //Current
+    var item = data.query.results.channel.item;
+    document.getElementById("currentWeatherText").innerText = item.condition.text;
+    document.getElementById("currentTemp").innerText = item.condition.temp;
+    
+
+
+    //For the day
+    document.getElementById("highTemp").innerText = data.query.results.channel.item.forecast[0].high;
+    document.getElementById("lowTemp").innerText = data.query.results.channel.item.forecast[0].low;
+
+    document.getElementById("forcastText").innerText = data.query.results.channel.item.forecast[0].text;
+
+    document.getElementById("getHumidity").innerText = data.query.results.channel.atmosphere.humidity;
+
+    var wind = data.query.results.channel.wind;
+    document.getElementById("windInfo").innerHTML = "Wind Chill: " + wind.chill + "&nbsp; / &nbsp;Wind Speed:  " + wind.speed;
+
+};
 
 
 
 
+//--Moon API--
 
 
 
 
+//--TOP NEWS API--
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=c835c5821eec41829538c121edd4e178';
+var req = new Request(url);
+fetch(req)
+    .then(function (response) {
+        console.log(response.json());
 
+        let articles = response.articles;
+        for (var i = 0; i < articles.totalResults; i++) {
+            document.getElementById("topNews").innerHTML =
+            // '<p class = "w3-section w3-container w3-card w3-dark-grey w3-cell w3-mobile w3-col m8">' +
+            '<p>' + articles[i].title + '</p>';
+            // +articles.title + <br>
+            // +articles.description + <br>
+            // +articles.urlToImage + <br>
+            // +articles.url
+            // + '</p>';
+        };
+    })
 
-
+    
